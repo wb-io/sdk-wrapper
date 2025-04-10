9ea18d8461d86b610b1fd27ec08a1b801a4fb392
Artyom Ponkratenkov <a.ponkratenkov@whitebird.io>
2025-04-10 14:21:35 +0300

    bugfix/CRYP-7267: Updated url params transfer
    https://jira.corp.whitebird.io/browse/CRYP-7267

--- web/lib/wbExchangeSdk-v001.js
+++ web/lib/wbExchangeSdk-v001.js
@@ -152,7 +152,7 @@
     };
 
     const queryString = Object.entries(params)
-        .filter(([, value]) => value !== undefined && value !== null && value !== false)
+        .filter(([, value]) => Boolean(value))
         .map(([key, value]) => `${key}=${encodeURIComponent(value)}`)
         .join("&");
 
