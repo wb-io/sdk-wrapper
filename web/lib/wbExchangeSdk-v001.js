595099a5f455cfebe909a39a8efc7f8a59249b88
Artyom Ponkratenkov <a.ponkratenkov@whitebird.io>
2025-07-10 17:53:18 +0300

    feature/CRYP-7748: Added tokens transfer by postMessage in LoginMode
    https://jira.corp.whitebird.io/browse/CRYP-7748

--- web/lib/wbExchangeSdk-v001.js
+++ web/lib/wbExchangeSdk-v001.js
@@ -181,6 +181,8 @@
     {
       config.onUserDataHandler?.({
         email: data?.email,
+        accessToken: data?.accessToken,
+        refreshToken: data?.refreshToken,
       });
     }
     if (data?.type === PostMessageType.OnBackButton)
