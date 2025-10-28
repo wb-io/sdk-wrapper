95719a28ffedefa9ef5847c32110d7dd3de592a5
Vitaliy <79109670+Ramosok@users.noreply.github.com>
2025-10-28 14:12:07 +0300

    feat/CRYP-8534: feat add redirect to signup on SDK (#31)
    Co-authored-by: v.maslyonik <v.maslyonik@easyrocket.net>

---
+++ web/lib/wbExchangeSdk.jsdoc.js
@@ -0,0 +1,62 @@
+/**
+ * @fileoverview Документация WBExchange SDK
+ * @namespace wbExchangeSdk
+ */
+
+/**
+ * Режимы работы SDK
+ * @typedef {Object} SdkMode
+ * @property {string} AuthMode - Режим авторизации
+ * @property {string} LoginMode - Режим логина
+ * @property {string} TokensMode - Режим с токенами
+ */
+
+/**
+ * Параметры колбэка onLogin
+ * @typedef {Object} LoginCallbackParams
+ * @property {string} email - Email пользователя
+ * @property {string} accessToken - Токен доступа
+ * @property {string} refreshToken - Токен обновления
+ * @property {boolean} isUserVerified - Статус верификации
+ */
+
+/**
+ * Конфигурация SDK
+ * @typedef {Object} SetupConfig
+ * @property {HTMLElement} el - HTML-элемент контейнера
+ * @property {string} mode - Режим работы
+ * @property {string} merchantId - ID мерчанта
+ * @property {string} [accessToken] - Токен доступа
+ * @property {string} [refreshToken] - Токен обновления
+ * @property {string} [email] - Email
+ * @property {string} [merchantPass] - Пароль мерчанта
+ * @property {string} [externalClientId] - Внешний ID клиента
+ * @property {number} [currencyAmount] - Сумма обмена
+ * @property {string} [currencyFrom] - Исходная валюта
+ * @property {string} [currencyTo] - Целевая валюта
+ * @property {boolean} [disableCurrencyFrom] - Отключить валюту from
+ * @property {boolean} [disableCurrencyTo] - Отключить валюту to
+ * @property {string} [cryptoWallet] - Крипто-кошелек
+ * @property {string} [redirectUrl] - URL перенаправления
+ * @property {string} [startAppPage] - Стартовая страница
+ * @property {boolean} [showBackButtonOnHomePage] - Кнопка назад
+ * @property {boolean} [disableAddCard] - Отключить карты
+ * @property {boolean} [debug] - Режим отладки
+ * @property {function(LoginCallbackParams):void} [onLogin] - Колбэк логина
+ * @property {function} [onUserData] - Колбэк данных пользователя
+ * @property {function} [onOrderCreated] - Колбэк создания заказа
+ * @property {function} [onExit] - Колбэк выхода
+ */
+
+/**
+ * Инициализация SDK
+ * @function setup
+ * @memberof wbExchangeSdk
+ * @param {SetupConfig} config - Конфигурация
+ */
+
+/**
+ * Очистка SDK
+ * @function cleanup
+ * @memberof wbExchangeSdk
+ */
