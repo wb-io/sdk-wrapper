b8b450fbb205513d2953431932feb78e869f86f8
vitaliy.maslionik <v.maslenik@whitebird.io>
2026-01-19 11:53:55 +0300

    fix/INTEG-175: fix carusell

--- web/lib/wbExchangeSdk-v001.js
+++ web/lib/wbExchangeSdk-v001.js
@@ -167,19 +167,10 @@
     window.addEventListener("message", onPostMessageHandler);
 
     function openFromTelegramTop(url) {
-      console.info({ url });
-      const tg = window.Telegram?.WebApp;
-      if (tg?.openLink) {
-        console.info("Telegram", window.Telegram);
-        tg.openLink(url);
-        return true;
-      }
-      console.info("else");
+      localStorage.setItem("url", url);
       window.location.href = url;
-      return true;
     }
 
-    console.info("makeIframe");
     window.addEventListener("message", (e) => {
       if (e.data?.type === "TG_OPEN_LINK" && typeof e.data.url === "string") {
         openFromTelegramTop(e.data.url);
