c984ae5d0de07f59678a1305b563e1ffaf5eae3a
vitaliy.maslionik <v.maslenik@whitebird.io>
2026-01-19 15:45:26 +0300

    fix/INTEG-175: fix carusell

--- web/lib/wbExchangeSdk-v001.js
+++ web/lib/wbExchangeSdk-v001.js
@@ -34,7 +34,7 @@
   // ----------------------------------------------------
   const version = "/v2.0";
 
-  const SDK_ORIGIN = "https://sdk.qa.wbdevel.net";
+  const SDK_ORIGIN = "https://sdk.dev.wbdevel.net";
   const SDK_URL = SDK_ORIGIN + version;
 
   //Only local test
