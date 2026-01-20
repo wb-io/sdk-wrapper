fee5944fce3f1ce8e609720edc4672e79e93bc8a
Vitaliy <79109670+Ramosok@users.noreply.github.com>
2026-01-20 07:08:46 +0300

    fix/INTEG-175: fix carusell (#62)
    Co-authored-by: vitaliy.maslionik <v.maslenik@whitebird.io>

--- web/lib/wbExchangeSdk-v001.js
+++ web/lib/wbExchangeSdk-v001.js
@@ -165,7 +165,7 @@
       const tg = window.Telegram?.WebApp;
 
       if (tg?.openLink) {
-        tg.openLink(url, { try_instant_view: false });
+        tg.openLink(url, { tryInstantView: true });
       } else {
         window.open(url, "_blank", "noopener,noreferrer");
       }
