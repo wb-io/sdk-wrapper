58f61ef14a8fdcea6a0bc886a5fe49b817482f71
Vitaliy <79109670+Ramosok@users.noreply.github.com>
2026-03-31 10:40:32 +0300

    feat/INTEG-487: add referal link (#83) (#84)
    Co-authored-by: vitaliy.maslionik <v.maslenik@whitebird.io>

--- web/lib/wbExchangeSdk-v001.js
+++ web/lib/wbExchangeSdk-v001.js
@@ -78,6 +78,7 @@
     disableAddCard: false,
     onOrderCreatedHandler: undefined,
     onExitHandler: undefined,
+    isTgBot: false,
   };
 
   let config = Object.assign({}, defaultConfig);
@@ -89,6 +90,8 @@
       const logColor = "background:#ff0;color:#000;";
       console.info(`%c wbExchangeSdkConfig`, logColor, params);
     }
+
+    config.isTgBot = params.isTgBot;
 
     if (params.el && config.el !== params.el && config.sdkIframe) {
       config.sdkIframe?.parentElement?.removeChild(config.sdkIframe);
