e7fe54e3f55e22bad80ec8e543ac55002f4721c3
vitaliy.maslionik <v.maslenik@whitebird.io>
2026-06-30 09:58:22 +0300

    feat/INTEG: add castomise

--- web/lib/sdkDemo.js
+++ web/lib/sdkDemo.js
@@ -12,6 +12,9 @@ const DEFAULT_CONFIG = {
   accessToken: "",
   refreshToken: "",
 
+  themePrimary: "#0169ff",
+  themeFontFamily: "Inter",
+
   email: "",
   merchantPass: "",
   externalClientId: "",
@@ -78,7 +81,19 @@ const FIELD_DEFS = [
     valueClass: (v) => (v?.length ? "hasValue" : ""),
     diff: false,
   },
-
+  {
+    prop: "themePrimary",
+    inputId: "themePrimary",
+    valueId: "themePrimaryValue",
+    kind: "text",
+    trim: true,
+  },
+  {
+    prop: "themeFontFamily",
+    inputId: "themeFontFamily",
+    valueId: "themeFontFamilyValue",
+    kind: "select",
+  },
   {
     prop: "email",
     inputId: "email",
